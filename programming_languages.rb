3cd22768a2850361904231c0a2692e47b13f0b54
Christopher Weber <chrisrrweber@gmail.com>
2018-09-13 15:35:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,41 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+def language_helper
+  language_hash.each do |language_category, languages|
+  languages.each do |language, characteristics|
+  characteristics.each do |characteristic, value|
+end
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
 end
